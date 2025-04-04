b64ef25f61cf1ce72f53a6ee602cef30bde8672d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 15:11:14 +0300

    Update UserService.java

--- smart-wallet-application/src/main/java/app/user/service/UserService.java
+++ smart-wallet-application/src/main/java/app/user/service/UserService.java
@@ -1,12 +1,13 @@
 package app.user.service;
 
 import app.user.model.User;
+import jakarta.servlet.Registration;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    public User register() {
+    public User register(Registration registration) {
 
         return null;
     }
