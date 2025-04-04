c8722c00667953032c83aaff7e86ebbbda68f63e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 15:03:11 +0300

    Create RegisterRequest.java

---
+++ smart-wallet-application/src/main/java/app/web/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package app.web.dto;
+
+import app.user.model.Country;
+
+public class RegisterRequest {
+
+    private String username;
+
+    private String password;
+
+    private Country country;
+
+
+}
