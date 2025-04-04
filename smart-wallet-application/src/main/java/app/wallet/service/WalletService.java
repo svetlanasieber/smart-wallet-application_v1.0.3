12a0b455ab9f342f82f1c2f3b9f8e9cdaa355c7f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 18:56:43 +0300

    Update WalletService.java

--- smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
+++ smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
@@ -1,7 +1,13 @@
 package app.wallet.service;
 
+import app.user.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WalletService {
+
+    public void createNewWallet(User user) {
+
+        
+    }
 }
