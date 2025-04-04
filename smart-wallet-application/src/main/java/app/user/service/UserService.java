3c9246c1874dccb7dbc6f65f0b7d98275fef3825
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 15:04:23 +0300

    Create UserService.java

---
+++ smart-wallet-application/src/main/java/app/user/service/UserService.java
@@ -0,0 +1,14 @@
+package app.user.service;
+
+import app.user.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User register() {
+
+        return null;
+    }
+
+}
